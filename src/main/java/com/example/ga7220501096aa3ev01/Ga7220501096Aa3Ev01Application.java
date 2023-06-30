7aaae1285d4123e97c7f77f0bd8b0dc8be2e49c5
Stiven M <stvnm33@gmail.com>
2023-06-30 17:00:09 -0500

    Creando el proyecto

---
+++ src/main/java/com/example/ga7220501096aa3ev01/Ga7220501096Aa3Ev01Application.java
@@ -0,0 +1,13 @@
+package com.example.ga7220501096aa3ev01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ga7220501096Aa3Ev01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ga7220501096Aa3Ev01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ga7220501096aa3ev01/Ga7220501096Aa3Ev01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ga7220501096aa3ev01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ga7220501096Aa3Ev01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
